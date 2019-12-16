552c63f4ad69df2622394697bab6e50d330a775c
Eric Enomoto <erice@lagash.com>
2019-12-16 15:29:37 -0300

    Se agrega archivos en el gitignore y se descomenta test

--- src/test/java/test/Case_Login.java
+++ src/test/java/test/Case_Login.java
@@ -28,26 +28,26 @@ public class Case_Login extends BaseTest {
 	/**
 	 * Test de usuario bloqueado
 	 */
-//	@Test
-//	public void lockedLogin() {
-//		log.info("Inicio de prueba de login de un usuario bloqueado");
-//		Login loginPage = new Login(driver, driverWait, log);
-//		loginPage.login(data.get_lockedUser(), data.get_password());
-//		loginPage.checkMessage(data.get_lockedMessage());
-//		log.info("Se finaliza el test de usuario bloqueado");
-//	}
-//	
-//	/**
-//	 * Test de contraseña incorrecto
-//	 */
-//	@Test
-//	public void wrongPassword() {
-//		log.info("Inicio de prueba de login con contraseña incorrecta");
-//		Login loginPage = new Login(driver, driverWait, log);
-//		loginPage.login(data.get_user(), "HolaMundo");
-//		loginPage.checkMessage(data.get_wrongUserPass());
-//		log.info("Se finaliza el test de contraseña incorrecta");
-//	}
+	@Test
+	public void lockedLogin() {
+		log.info("Inicio de prueba de login de un usuario bloqueado");
+		Login loginPage = new Login(driver, driverWait, log);
+		loginPage.login(data.get_lockedUser(), data.get_password());
+		loginPage.checkMessage(data.get_lockedMessage());
+		log.info("Se finaliza el test de usuario bloqueado");
+	}
+	
+	/**
+	 * Test de contraseña incorrecto
+	 */
+	@Test
+	public void wrongPassword() {
+		log.info("Inicio de prueba de login con contraseña incorrecta");
+		Login loginPage = new Login(driver, driverWait, log);
+		loginPage.login(data.get_user(), "HolaMundo");
+		loginPage.checkMessage(data.get_wrongUserPass());
+		log.info("Se finaliza el test de contraseña incorrecta");
+	}
 	
 	@After
 	public void tearDown() {
