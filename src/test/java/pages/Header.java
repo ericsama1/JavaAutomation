f9f3d2d9ab3c25a66a98deef74f56261316c5bbd
Eric Enomoto <erice@lagash.com>
2020-01-03 14:25:18 -0300

    Add new method in the class, to select options on the menu

--- src/test/java/pages/Header.java
+++ src/test/java/pages/Header.java
@@ -1,16 +1,12 @@
 package pages;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.List;
 import org.apache.log4j.Logger;
 import pageFactory.AbstractPageObject;
-
-import org.openqa.selenium.By;
 
 public class Header extends AbstractPageObject{
 
@@ -20,17 +16,64 @@ public class Header extends AbstractPageObject{
 	
 	private By byMenuButton = By.xpath("//button[text()='Open Menu']");
 	private By byCartButton = By.xpath("//*[@data-icon='shopping-cart']");
+	private By byAllItems = By.id("inventory_sidebar_link");
+	private By byAbout = By.id("about_sidebar_link");
+	private By byLogout = By.id("logout_sidebar_link");
+	private By byReset = By.id("reset_sidebar_link");
 	
 	private WebElement menuButton = driver.findElement(byMenuButton);
 	private WebElement cartButton = driver.findElement(byCartButton);
+	private WebElement allItemsButton = driver.findElement(byAllItems);
+	private WebElement about = driver.findElement(byAbout);
+	private WebElement logout = driver.findElement(byLogout);
+	private WebElement reset = driver.findElement(byReset);
 	
+	
+	/**
+	 * Method to do a click on the menu button
+	 */
 	public void clickMenu() {
 		menuButton.click();
 		log.info("Se hace click sobre el botón del menu");
 	}
 	
+	/**
+	 * Method to do a click on the car button
+	 */
 	public void clickCat() {
 		cartButton.click();
 		log.info("Se hace click sobre el botón del carrito");
+	}
+	
+	/**
+	 * Method to do a click on the All Items button
+	 */
+	public void clickAllItems() {
+		allItemsButton.click();
+		log.info("Se hace click sobre el botón de All Items");
+	}
+	
+	/**
+	 * Method to do a click on the About button
+	 */
+	public void clickAbout() {
+		about.click();
+		log.info("Se hace click sobre el botón de About");
+	}
+	
+	/**
+	 * Method to do a click on the Logout button
+	 */
+	public void clickLogout() {
+		logout.click();
+		log.info("Se hace click sobre el botón de Logout");
+	}
+
+	/**
+	 * Method to do a click on the Reset button
+	 */
+	public void clickReset() {
+		reset.click();
+		log.info("Se hace click sobre el botón de Reset");
 	}
 }
