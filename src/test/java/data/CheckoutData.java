563376c8ffb83b3a2c0dbcaf4b671e3c8386e2ac
Eric Enomoto <erice@lagash.com>
2020-01-07 10:38:20 -0300

    Add the checkout's page

---
+++ src/test/java/data/CheckoutData.java
@@ -0,0 +1,20 @@
+package data;
+
+public class CheckoutData {
+
+	private static final String FIRSTNAME = "prueba";
+	private static final String LASTNAME = "Apellido";
+	private static final String POSTALCODE = "1234";
+	
+	public String getFirstName() {
+		return FIRSTNAME;
+	}
+	
+	public String getLastName() {
+		return LASTNAME;
+	}
+	
+	public String getPostalCode() {
+		return POSTALCODE;
+	}
+}
