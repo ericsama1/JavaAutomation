d48169c1eb86fc12b1c7018e69bc6f8b76f9c84d
Eric Enomoto <erice@lagash.com>
2020-01-09 15:43:36 -0300

    Created Product and Products class

---
+++ src/test/java/data/Product.java
@@ -0,0 +1,25 @@
+package data;
+
+public class Product {
+	private String name;
+	private String description;
+	private String price;
+
+	public Product(String name, String description, String price) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public String getPrice() {
+		return this.price;
+	}
+}
---
+++ src/test/java/data/Products.java
@@ -0,0 +1,89 @@
+package data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import data.Product;
+
+public class Products {
+
+	private List<Product> list = new ArrayList<Product>();
+	
+	public Products() {
+		Product backpack = new Product(
+ 			   "Sauce Labs Blackpack", 
+               "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising "
+               + "style with unequaled laptop and tablet protection.",
+               "$29.99");
+		Product bikeLight = new Product(
+				"Sauce Labs Bike Light",
+				"A red light isn't the desired state in testing but it sure helps when riding your "
+				+ "bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.",
+				"$9.99");
+		Product tShirt = new Product(
+				"Sauce Labs Bolt T-Shirt",
+				"Get your testing superhero on with the Sauce Labs bolt T-shirt. From American "
+				+ "Apparel, 100% ringspun combed cotton, heather gray with red bolt.", 
+				"$15.99");
+		Product jacket = new Product(
+				"Sauce Labs Fleece Jacket",
+				"It's not every day that you come across a midweight quarter-zip fleece jacket "
+				+ "capable of handling everything from a relaxing day outdoors to a busy day "
+				+ "at the office.",
+				"$49.99");
+		Product onesie = new Product(
+				"Sauce Labs Onesie", 
+				"Rib snap infant onesie for the junior automation engineer in development. "
+				+ "Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom "
+				+ "won't unravel.", 
+				"$7.99");
+		Product tShirtRed = new Product(
+				"Test.allTheThings() T-Shirt (Red)",
+				"This classic Sauce Labs t-shirt is perfect to wear when cozying up to your "
+				+ "keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton.",
+				"$15.99");
+		list.add(backpack);
+		list.add(bikeLight);
+		list.add(tShirt);
+		list.add(jacket);
+		list.add(onesie);
+		list.add(tShirtRed);
+	}
+	
+	public String getName(int index) {
+		Product prod = list.get(index);
+		return prod.getName();
+	}
+	
+	public String getDescription(int index) {
+		Product prod = list.get(index);
+		return prod.getDescription();
+	}
+
+	public String getPrice(int index) {
+		Product prod = list.get(index);
+		return prod.getPrice();
+	}
+	
+	public String getDescription(String name) {
+		String desc = null;
+		for (Product product: list) {
+			if (product.getName().equals(name)) {
+				desc = product.getDescription();
+				break;
+			}
+		}
+		return desc;
+	}
+	
+	public String getPrice(String name) {
+		String price = null;
+		for (Product product: list) {
+			if (product.getName().equals(name)) {
+				price = product.getPrice();
+				break;
+			}
+		}
+		return price;
+	}
+}
